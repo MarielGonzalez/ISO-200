e733d145ba5c9b73a9b4de7c8c8895df587f0846
MarielGonzalez <47929216+MarielGonzalez@users.noreply.github.com>
2021-09-26 19:46:09 +0000

    En este commit se cambiaron terminos del ingles al español. Se ajusto la porcion total por 5 dias. Se cambiaron el nombre de algunas variables.

--- main.cpp
+++ main.cpp
@@ -1,41 +1,41 @@
 #include <iostream>
 
+int perritasNoEmb = 3 ;
+int perritasEmb = 2 ;
+int totalPerritas= perritasNoEmb+perritasEmb;
+
+const double porcionNormal = 0.25;
+const double porcionMañana = 0.25 * 2;
+const double tarde = 2;
+const double porcionEmb = (porcionNormal * 1.04) * tarde;
+
+
 
 int main() {
 //Determinar la cantidad de purina que cada perrita consume>
-
-int perritasNoPreg = 3 ;
-int perritasPreg = 2 ;
-int totalPerritas= perritasNoPreg+perritasPreg;
-
-const double nomalFood = 0.25;
-const int morning  = 2;
-const int afternoon  = 2;
-const double pregancyfood = ((nomalFood * 4/100) + nomalFood) * afternoon;
-
   std::cout << "Practica Perritas\n";
 
- double normalFeed= ((nomalFood * morning) + (nomalFood * afternoon)) * perritasNoPreg;
+ double porcionDiariaNoEmb = perritasNoEmb * (porcionMañana + (porcionNormal * tarde));
 
- double pregancyFeed= (pregancyfood + (nomalFood * morning)) *perritasPreg ;
+ double porcionDiariaEmb= perritasEmb * (porcionEmb + porcionMañana)  ;
 
   //1. La cantidad de purina que comen las que no están embarazadas
 
   std::cout << "Cuanto comen las perritas que no estan embarazdas?\n" ;
 
-  std::cout << "Las que no estan embarazdas comen: \n" <<  (normalFeed) << "Lb de purina"   << '\n';
+  std::cout << "Las que no estan embarazdas comen: \n" <<  (porcionDiariaNoEmb) << "Lb de purina"   << '\n';
 
  //2. La cantidad de purina que comen las que están embarazadas
   
-  std::cout << "Cuanto comen las perritas embarazdas?\n";
+std::cout << "Cuanto comen las perritas embarazdas?\n";
 
-std::cout << "Las que estan embarazdas comen: \n" <<  (pregancyFeed) << "Lb de purina"   << '\n';
+std::cout << "Las que estan embarazdas comen: \n" <<  (porcionDiariaEmb) << "Lbs de purina"   << '\n';
 
  //1. La cantidad de purina total que comen las perritas en 5 dias
 
- double totalPurinaFiveDays= ((normalFeed + pregancyFeed) * totalPerritas)* 5 ;
+ double porcionTotal_5dias= (porcionDiariaNoEmb + porcionDiariaEmb)* 5 ;
 
- std::cout << "En 5 dias, las perritas comen un total de: \n" <<  (totalPurinaFiveDays) << "Lb de purina"   << '\n';
+ std::cout << "En 5 dias, las perritas comen un total de: \n" <<  (porcionTotal_5dias) << "Lbs de purina"   << '\n';
 
   
 
