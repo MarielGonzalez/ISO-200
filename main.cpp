54df7d66d65b0963ab6ca9277873d0b8d563583d
MarielGonzalez <47929216+MarielGonzalez@users.noreply.github.com>
2021-09-27 23:29:29 +0000

    fix: adding namespace std

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+using namespace std;
+
 int perritasNoEmb = 3 ;
 int perritasEmb = 2 ;
 int totalPerritas= perritasNoEmb+perritasEmb;
@@ -11,18 +13,18 @@ const double porcionEmb = (porcionNormal * 1.04) * tarde;
 
 int main() {
 	//Determinar la cantidad de purina que cada perrita consume>
-	std::cout << "Tarea 4 - Perritas:"<< "\n \n";
+	cout << "Tarea 4 - Perritas:"<< "\n \n";
 	
 	double porcionDiariaNoEmb = perritasNoEmb * (porcionManana + (porcionNormal * tarde));
 	double porcionDiariaEmb= perritasEmb * (porcionEmb + porcionManana)  ;
 	
 	//1. La cantidad de purina que comen las que no están embarazadas
-	std::cout << "Las perritas que no estan embarazadas comen:\t\t" <<  (porcionDiariaNoEmb) << " Lbs de purina"   << '\n';
+	cout << "Las perritas que no estan embarazadas comen:\t\t" <<  (porcionDiariaNoEmb) << " Lbs de purina"   << '\n';
 	
 	//2. La cantidad de purina que comen las que están embarazadas
-	std::cout << "Las perritas que estan embarazadas comen:\t\t"<<  (porcionDiariaEmb) << " Lbs de purina"   << "\n \n" ;
+	cout << "Las perritas que estan embarazadas comen:\t\t"<<  (porcionDiariaEmb) << " Lbs de purina"   << "\n \n" ;
 	
 	//1. La cantidad de purina total que comen las perritas en 5 dias
 	double porcionTotal_5dias= (porcionDiariaNoEmb + porcionDiariaEmb)* 5 ;
-	std::cout << "En 5 dias, las perritas comen un total de:\t\t" <<  (porcionTotal_5dias) << " Lbs de purina"   << '\n';
+	cout << "En 5 dias, las perritas comen un total de:\t\t" <<  (porcionTotal_5dias) << " Lbs de purina"   << '\n';
 }
